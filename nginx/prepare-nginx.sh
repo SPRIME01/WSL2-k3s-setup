d35110c8d30c8e352a97d4f84a16192e7f5ea4c1
primefam <primeevery1@gmail.com>
2025-02-14 14:29:03 -0500

    Replace Nginx with Traefik for reverse proxy setup in Docker

--- nginx/prepare-nginx.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo ">>>>> Preparing Nginx as reverse proxy"
-## Prepare Nginx as local reverse proxy between localhost and WSL internal network
-sudo apt update && sudo apt install nginx -y
-sudo cp nginx.conf /etc/nginx
-sudo service nginx restart
-echo "<<<<< Nginx ready."
